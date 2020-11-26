30c2031323769e7bc4660e1c29826209bc744098
IMB Microscopy <52775670+IMBMicroscopy@users.noreply.github.com>
2020-11-26 14:49:38 +1000

    Update b_initialisation.ino

--- AMC/b_initialisation.ino
+++ AMC/b_initialisation.ino
@@ -2,7 +2,7 @@
 #include <avr/eeprom.h>  //read/write to internal EEPROM chip
 
 String softVer = "AMC_1/11/19";  //Software version 
-String serialNumber = "0012";  //Hardware Serial number
+String serialNumber = "008";  //Hardware Serial number
 
 // define the different operating modes of the relay unit
 #define startMode     0
