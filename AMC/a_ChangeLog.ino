a1549a9581e4a22e0bd44a3a1fc0029460fc8fe7
IMB Microscopy <52775670+IMBMicroscopy@users.noreply.github.com>
2021-08-24 08:34:09 +1000

    Update a_ChangeLog.ino

--- AMC/a_ChangeLog.ino
+++ AMC/a_ChangeLog.ino
@@ -1,5 +1,6 @@
 /*
  23/8/21 - Added Lamp Minutes recording 
+  The relay now records lamp minutes used, and saves them to eeprom when they hit 60min increments, at cpuReset or at the start of offMode
 
  
  */
