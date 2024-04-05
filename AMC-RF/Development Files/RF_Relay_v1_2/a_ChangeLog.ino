1fe68396b08b76d553bd9f4bb5340533e0e4ed9d
JS <j.springfield@uq.edu.au>
2024-04-05 10:05:50 +1000

    tidy up

--- AMC-RF/Development Files/RF_Relay_v1_2/a_ChangeLog.ino
+++
@@ -1,7 +0,0 @@
-/*
- 23/8/21 - Added Lamp Minutes recording 
-  The relay now records lamp minutes used, and saves them to eeprom when they hit 60min increments, at cpuReset or at the start of offMode
-
- 11/11/21 - Added case statement in relay_initialisation to deal with various hardware pinouts on different AMCs
- 
- */
--- AMC/Installation Files/WinXPSP3/ScopeConfig/3 - Executables/relay_message.js
+++
@@ -1,9 +0,0 @@
-WScript.Echo("If the Microscope power doesn't come on after log in, double click 'SCOPE ON' on the desktop\n" +"\n" 
-+"To Start the Fluorescence Lamp, Double Click 'LAMP ON' on the desktop\n" +"\n" 
-+"If the Lamps are in Cooling mode, then you may have to wait up to 5 minutes before the Lamps will auto-start,\n" +"\n"
-+"If after 5 minutes they dont start, then double click the 'LAMP ON' Button again\n" +"\n"
-+"The Fluorescence Lamp will stay on for a minimum of 30mins and go into Auto-shutdown after 1hr30mins\n" +"\n"
-+"The Microscope power will also go into Auto-shutdown after 1hr30mins\n" +"\n"
-+"Once Auto-Shutdown has begun the unit will beep for 15 seconds, you have 3 minutes to double click the 'LAMP ON' button to continue using the fluorescence lamp,\n" +"\n"
-+"Or you can double click the 'SCOPE ON' button to continue using just the microscope,\n" +"\n"
-+"Otherwise the unit will shutdown the lamps and go into cooling mode, once in cooling mode, you wont be able to start the Fluorescence Lamp for 5mins\n");
